ac76c8f3f4a8b468614cb2f9af68e21debb858ce
martin-call-learning <martin@call-learning.fr>
2023-03-09 16:28:31 +0100

    Fixed all tests failing on moodle 3.9

--- tests/questiontypes_test.php
+++ tests/questiontypes_test.php
@@ -29,6 +29,7 @@ use mod_questionnaire\question\question;
 
 global $CFG;
 require_once($CFG->dirroot.'/mod/questionnaire/locallib.php');
+require_once($CFG->dirroot.'/mod/questionnaire/classes/question/question.php'); // Import type question ids.
 
 /**
  * Unit tests for {@link questionnaire_questiontypes_testcase}.
